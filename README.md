1f113ae5c9f24588b18b9865121f5dc1a2bab169
Tyler D <143891068+bbunny27@users.noreply.github.com>
2025-03-05 01:19:39 -0600

    Added Documentation/Formatting
    I added documentation to several files and updated the README.md file.

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 ## This folder is for Assignment 3 in ITSCM 451.
-Completed by Tyler Deal
-testing
+Completed by Tyler Deal.
+
+This assignment utilized several AWS services to create a CICD Pipeline for a python application that automatically pull information from a Github repository, and then deploys it to an EC2 instance.   
+  
+Completed on 3/5/2025
--- scripts/start.sh
+++ scripts/start.sh
@@ -4,3 +4,5 @@ echo 5 | python3 prime.py > /home/ubuntu/prime/output.txt 2>&1
 # I changed how the Python program was being called in this file. The python program provided for us is coded in a way that expects a human input, and does not accept passing an argument in the way it was originally set up. 
 # Originally the section was "python3 prime.py 5". If I echo "5" and pipe it into prime.py instead of attempting to pass the argument the other way, prime.py will accept the 5 as human input. 
 # Alternatively, I could have changed the way input is happening in the python file, but I found this to be the easiest solution.
+
+# I utilized AI to reach this solution, however, I feel comfortable enough with bash scripting that I understand what is happening.
